65857367d9e94743ed74f20c48bd1d5702a0c9e1
Md Sufi Khan <sufi.du@gmail.com>
2022-12-30 19:05:44 +0600

    Created animated action widget. The color of the text will change while hovering.

---
+++ lib/widgets/animated_action_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AnimatedActionWidget extends StatefulWidget {
+  final String label;
+  const AnimatedActionWidget({super.key, required this.label});
+
+  @override
+  State<AnimatedActionWidget> createState() => _AnimatedActionWidgetState();
+}
+
+class _AnimatedActionWidgetState extends State<AnimatedActionWidget> {
+  bool _isHover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onHover: (value) {
+        setState(() {
+          _isHover = value;
+        });
+      },
+      onTap: () {},
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: Text(
+          widget.label,
+          style: TextStyle(
+              color: _isHover ? Colors.orange.shade400 : Colors.white,
+              fontSize: 17),
+        ),
+      ),
+    );
+  }
+}
